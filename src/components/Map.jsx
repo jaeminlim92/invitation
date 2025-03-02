863386a48a17517a09d1abad97130b0725653a89
jaemin lim <jaminlim159@gmail.com>
2025-03-02 22:32:07 +0900

    GPG TEST“

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -46,7 +46,7 @@ function Map() {
       </div>
       <div className="content">
         <ul><br/>2025년 9월 20일 오후 5시 30분</ul>
-        <ul>서울 동대문구 회기로 56</ul>
+        <ul>서울 동대문구 회기로 56 test</ul>
         <ul>세종대왕 기념관</ul>
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
